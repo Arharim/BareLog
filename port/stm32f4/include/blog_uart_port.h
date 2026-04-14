badbcc38f50428aa4d333a09605e8bec41702f42
Danil <145000945+Arharim@users.noreply.github.com>
2026-04-14 22:43:17 +0500

    feat: parameterize STM32F4 port for all F4 variants (40+ chips)
    - Download CMSIS headers for all F4 sub-families (F401-F479)
    - Download startup files for all 21 device variants
    - Parameterized target.cmake: -DSTM32F4_CHIP=STM32F429VI selects device,
      startup, linker (Flash/RAM), HCLK, UART clock automatically
    - Replace static linker script with template (configure_file)
    - All defines match ST CMSIS format (STM32F429xx, STM32F401xC, etc.)
    - Build: cmake -DUHAL_TARGET=stm32f4 -DSTM32F4_CHIP=STM32F446RE

--- port/stm32f4/include/blog_uart_port.h
+++ port/stm32f4/include/blog_uart_port.h
@@ -2,24 +2,59 @@
 #define BLOG_UART_PORT_H
 
 /*
- * STM32F407VGT6 UART DMA configuration
+ * STM32F4 UART DMA default configuration
  * USART2 on PA2/PA3, DMA2 Stream6 Channel 4
- * 84 MHz APB1 clock
+ *
+ * Override by defining BLOG_UART_CLOCK_HZ before inclusion
+ * or redefining these macros in your build.
  */
 
-#define BLOG_UARTx         USART2
-#define BLOG_UART_RCC_BIT  RCC_APB1ENR_USART2EN
-#define BLOG_UART_RCC_BUS  APB1ENR
-#define BLOG_UART_GPIO     GPIOA
-#define BLOG_UART_GPIO_RCC RCC_AHB1ENR_GPIOAEN
-#define BLOG_UART_TX_PIN   2u
-#define BLOG_UART_RX_PIN   3u
-#define BLOG_UART_AF       7u
-#define BLOG_UART_CLOCK_HZ 84000000u
+#ifndef BLOG_UARTx
+#	define BLOG_UARTx USART2
+#endif
 
-#define BLOG_UART_DMA_CHANNEL     DMA2_Stream6
-#define BLOG_UART_DMA_IRQn        DMA2_Stream6_IRQn
-#define BLOG_UART_DMA_TCIF_BIT    (1u << 22u)
-#define BLOG_UART_DMA_CHANNEL_SEL 4u
+#ifndef BLOG_UART_RCC_BIT
+#	define BLOG_UART_RCC_BIT RCC_APB1ENR_USART2EN
+#endif
+
+#ifndef BLOG_UART_GPIO
+#	define BLOG_UART_GPIO GPIOA
+#endif
+
+#ifndef BLOG_UART_GPIO_RCC
+#	define BLOG_UART_GPIO_RCC RCC_AHB1ENR_GPIOAEN
+#endif
+
+#ifndef BLOG_UART_TX_PIN
+#	define BLOG_UART_TX_PIN 2u
+#endif
+
+#ifndef BLOG_UART_RX_PIN
+#	define BLOG_UART_RX_PIN 3u
+#endif
+
+#ifndef BLOG_UART_AF
+#	define BLOG_UART_AF 7u
+#endif
+
+#ifndef BLOG_UART_CLOCK_HZ
+#	define BLOG_UART_CLOCK_HZ 84000000u
+#endif
+
+#ifndef BLOG_UART_DMA_CHANNEL
+#	define BLOG_UART_DMA_CHANNEL DMA2_Stream6
+#endif
+
+#ifndef BLOG_UART_DMA_IRQn
+#	define BLOG_UART_DMA_IRQn DMA2_Stream6_IRQn
+#endif
+
+#ifndef BLOG_UART_DMA_TCIF_BIT
+#	define BLOG_UART_DMA_TCIF_BIT (1u << 22u)
+#endif
+
+#ifndef BLOG_UART_DMA_CHANNEL_SEL
+#	define BLOG_UART_DMA_CHANNEL_SEL 4u
+#endif
 
 #endif
